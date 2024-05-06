c4bde8be45e60c4ebd114f1f1da6292a4ecd2e93
ameschmeems <pucylek@wp.pl>
2024-05-07 00:49:42 +0200

    project 4 done

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -9,3 +9,47 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+// wait for key press
+(INIT)
+	@8192 // amount of 16bit chunks (32 * 256)
+	D=A
+	@chunk
+	M=D
+
+(KBD_WAIT)
+	@KBD
+	D=M
+	@FILL_BLACK
+	D;JNE
+	@FILL_WHITE
+	0;JMP
+
+(FILL_BLACK)
+	@SCREEN
+	D=A
+	@chunk
+	A=D+M
+	M=-1
+	@chunk
+	D=M
+	@INIT
+	D;JEQ
+	@chunk
+	M=M-1
+	@KBD_WAIT
+	0;JMP
+
+(FILL_WHITE)
+	@SCREEN
+	D=A
+	@chunk
+	A=D+M
+	M=0
+	@chunk
+	D=M
+	@INIT
+	D;JEQ
+	@chunk
+	M=M-1
+	@KBD_WAIT
+	0;JMP
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// set x to r1, initialize r2 to 0
+	@R1
+	D=M
+	@x
+	M=D
+	@R2
+	M=0
+(LOOP)
+// If x==0, break
+	@x
+	D=M
+	@END
+	D;JEQ
+	// r2 += r0
+	@R0
+	D=M
+	@R2
+	M=M+D
+	// decrement x
+	@x
+	M=M-1
+	// loop back
+	@LOOP
+	0;JMP
+(END)
